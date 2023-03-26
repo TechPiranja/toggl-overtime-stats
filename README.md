7607a1888b83b9e49d3a3975ead4cc311e82e6ea
Anja Stricker <anja.stricker@quickbirdstudios.com>
2023-03-26 20:50:15 +0200

    add readme notes for setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # toggl-overtime-stats
-This react.js project uses the toggl api to deliver stats about overtime
+This react.js project uses the toggl api to deliver stats about overtime.
+
+![The San Juan Mountains are beautiful!](/src/assets/toggl-overtime-stats-pic.png "This is how it looks currently‚")
+
+# Setup
+You need to create a .env file in the root of the project and paste your personal toggl api key:
+
+`VITE_TOGGL_API_KEY = 'yourPersonalApiCode'`
--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import _ from 'lodash'
 import { calculateTotalHoursPerWeek } from './services/getTogglReport'
+import moment from 'moment'
 
 function App() {
   const [data, setData] = useState<any[]>([]);
-  const startDate = '2023-03-01';
-  const endDate = '2023-03-31';
+  const startDate = moment().startOf('month');
+  const endDate = moment().endOf('month');
 
   useEffect(() => {
-    calculateTotalHoursPerWeek(startDate, endDate).then(res => setData(res));
+    calculateTotalHoursPerWeek(startDate.toISOString(), endDate.toISOString()).then(res => setData(res));
     return () => { }
   }, [])
 
@@ -30,16 +31,14 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Toggl Overtime Calculator</h1>
+      <p>For {endDate.format('MMMM')}</p>
       <div className="card">
         {data?.map(week =>
-          <p key={week.weekNumber}>{`Week ${week.weekNumber}: ${(week.hours - 40).toFixed(2)} hours of overtime`}</p>
+          <p key={week.weekNumber}>{`Week ${week.weekNumber}: ${(week.hours - 40).toFixed(2)} h of overtime`}</p>
         )}
-        <p>Total overtime this month: {calculateTotalOvertime()}</p>
+        <p style={{ fontWeight: 'bold', fontSize: 20 }}>Total overtime this month: {calculateTotalOvertime()}</p>
       </div>
     </div>
   )
